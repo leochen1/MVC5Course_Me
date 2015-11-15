1fc875fef77d80c3a5214c753445e70da0d1ec0f
leochen <leocmc0723@hotmail.com>
2015-11-15 22:26:44 +0800

    建立 ASP.NET_MVC5 課程版本

---
+++ MVC5Course/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using MVC5Course.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [在多個控制器中共用的ViewBag屬性]
+        public ActionResult Test()
+        {
+            System.Diagnostics.Debug.WriteLine("Test Action");
+
+            throw new Exception("BAD");
+
+            return View();
+        }
+
+        public ActionResult ViewTest(bool enable = true)
+        {
+            ViewBag.IsEnabled = enable;
+
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+
+            return View(data);
+        }
+    }
+}
---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,27 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        //[限制欄位值必須出現兩個數字1]
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
